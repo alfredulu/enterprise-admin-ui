54a11032ab830a9fe7099dfb35ecb3bc97831754
Alfred <alfredulu3328@gmail.com>
2026-01-26 04:25:24 +0100

    integrate React Query for data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,26 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./app/App";
 import "./index.css";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+      staleTime: 30_000,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
